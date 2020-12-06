bbf883bd95a7d43bc84fa5e93416633f9dc94234
dsmriti <iamsmritid@gmail.com>
2020-12-07 00:43:49 +0530

    Changes for filter

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpParams} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -9,29 +9,21 @@ export class ApiService {
   constructor(private http: HttpClient) { }
   baseURL = "https://api.spacexdata.com/v3/launches?limit=100";
   
-  launchSuccess():Observable<any> {
-    return this.http.get<any>(this.baseURL + "&launch_success=true", {responseType: 'json'});
-  }
-  launchLandSuccess():Observable<any> {
-    return this.http.get<any>(this.baseURL + "&launch_success=true&land_success=true", {responseType: 'json'});
-  }
-  getAllFilter():Observable<any> {
-    return this.http.get<any>(this.baseURL, { params: { launch_success: 'true' }});
-  }
   getAllItems():Observable<any> {
     return this.http.get<any>(this.baseURL, {responseType: 'json'});
   }
   encode_query_params(queryJson:any) {
     let keys = Object.keys(queryJson);
-    let keys_seperator = "?";
+    let keys_seperator = "&";
+    let url = this.baseURL;
     for(let i=0; i<keys.length; i++) {
-        this.baseURL += keys_seperator + keys[i] + "=" + queryJson[keys[i]];
-        keys_seperator = "&";
+      if (queryJson[keys[i]] != "") {
+        url += keys_seperator + keys[i] + "=" + queryJson[keys[i]];
+      }
     }
-    return this.baseURL;
+    return url;
   }
-  test(param):Observable<any> {
-    console.log(this.encode_query_params(param))
-    return this.http.get<any>(this.encode_query_params(param));
+  getfilter(url):Observable<any> {
+    return this.http.get<any>(url);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { ApiService } from './api.service';
 export class AppComponent implements OnInit {
   title = 'spaceX';
   spaceXarray = [];
-  content;
   constructor(private api:ApiService){}
   yearList: any[] = ['2006', '2007', '2008', '2009', '2010', '2011','2012','2013','2014','2015','2016','2017','2018','2019','2020'];
   successList: any[] = [
@@ -20,46 +19,54 @@ export class AppComponent implements OnInit {
       value:'false'
     }
   ]
+
+  filter_query_params = {
+    launch_year: "",
+    launch_success: "",
+    land_success: ""
+  };
+  
   ngOnInit() {
     this.api.getAllItems().subscribe(
       data => {
       this.spaceXarray = data;
       })
    }
+
+  get_land_success_val(item) {
+    if (item.length < 1) {
+      return false;
+    }
+    return item[0].land_success;
+  } 
   getLaunchYear(year){
-    let launchYear = this.api.encode_query_params({"launch_year":year});
-    this.api.test(launchYear).subscribe(
+   if (typeof(year) != undefined) {
+      this.filter_query_params.launch_year = year;
+    }
+    let launchYear = this.api.encode_query_params(this.filter_query_params);
+    this.api.getfilter(launchYear).subscribe(
       data => {
-        console.log(launchYear);
         this.spaceXarray = data;
-      })
+    })
   }
   getLandSuccess(land){
-    let landSuccess = this.api.encode_query_params({"land_success":land});
-    this.api.test(landSuccess).subscribe(
+    if (typeof(land) != undefined) {
+      this.filter_query_params.land_success = land;
+    }
+    let landSuccess = this.api.encode_query_params(this.filter_query_params);
+    this.api.getfilter(landSuccess).subscribe(
       data => {
-        console.log(landSuccess);
         this.spaceXarray = data;
-      })
+    })
   }
-  getLaunchSuccess(launch){
-    let launchSuccess = this.api.encode_query_params({"launch_success":launch});
-    this.api.test(launchSuccess).subscribe(
+  getLaunchSuccess(launch){ 
+    if (typeof(launch) != undefined) {
+      this.filter_query_params.launch_success = launch;
+    }
+    let launchSuccess = this.api.encode_query_params(this.filter_query_params);
+    this.api.getfilter(launchSuccess).subscribe(
       data => {
-        console.log(launchSuccess);
         this.spaceXarray = data;
-      })
+    })
   }
-  //function that filter for all 3 parameter
-  filter(year?,land?,launch?){
-    let launchYear = this.api.encode_query_params({"launch_year":year});
-    let landSuccess = this.api.encode_query_params({"land_success":land});
-    let launchSuccess = this.api.encode_query_params({"launch_success":launch});
-    this.api.test(launchYear).subscribe(
-      data => {
-        console.log(launchYear);
-        this.spaceXarray = data;
-      })
-  }
-
 }
